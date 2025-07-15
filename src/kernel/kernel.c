a396eb5d995d0452324ac4a4667dc30f811f97c8
Александр <58480227+KEYTRON@users.noreply.github.com>
2025-07-15 06:11:39 +0300

    Add simple cooperative multitasking

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,6 +4,15 @@
 #include "vga.h"
 #include "fs.h"
 #include "shell.h"
+#include "task.h"
+
+static void demo_task1() {
+    print_string("Task1 running\n");
+}
+
+static void demo_task2() {
+    print_string("Task2 running\n");
+}
 
 void kernel_main(void) {
     clear_screen();
@@ -13,6 +22,15 @@ void kernel_main(void) {
     print_string("Welcome to MyOS!\n");
     print_string("Kernel initialized.\n");
     init_keyboard();
+
+    tasking_init();
+    task_create(demo_task1);
+    task_create(demo_task2);
+    print_string("Starting simple multitasking demo...\n");
+    for (int i = 0; i < 5; i++) {
+        task_run_once();
+    }
+
     print_string("Starting shell...\n");
     shell();
     while (1) {}
